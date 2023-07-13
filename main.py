5d5a7090c33fa527d820a60de30d3d9864d94a77
arcticronin <ronin.git@gmail.com>
2023-07-13 18:40:43 +0200

    backspacing instead of entering

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pyautogui
 
-pyautogui.click(50,50)
+pyautogui.click(50, 50)
 
-while(True):
-    pyautogui.write('AutoDucker in control! Duck The System!', interval=0.25)
-    pyautogui.press('enter')
+while True:
+    st = "AutoDucker in control! Duck The System!"
+    pyautogui.write(st, interval=0.25)
+    for i in st:
+        pyautogui.press("backspace")
 
-#tkinter
+# tkinter
